dd2454b7fc65fdf047aee6c9d911d87db2afa1ab
Mohit Rawat <mohit.rawat@factset.com>
2022-12-02 05:58:40 +0530

    Adding first draft

---
+++ base-domains/src/main/java/com/mor/basedomains/dto/OrderEvent.java
@@ -0,0 +1,49 @@
+package com.mor.basedomains.dto;
+
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+
+    public OrderEvent() {
+    }
+
+    public OrderEvent(String message, String status, Order order) {
+        this.message = message;
+        this.status = status;
+        this.order = order;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderEvent{" +
+                "message='" + message + '\'' +
+                ", status='" + status + '\'' +
+                ", order=" + order +
+                '}';
+    }
+}
