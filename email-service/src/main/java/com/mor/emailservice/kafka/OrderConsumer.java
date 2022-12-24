14e0a16f16b2b8a4dbc5a578e530a5649665c562
mohitrawat22 <45097724+mohitrawat22@users.noreply.github.com>
2022-12-24 10:54:33 +0530

    Logic for KafkaListener

--- email-service/src/main/java/com/mor/emailservice/kafka/OrderConsumer.java
+++ email-service/src/main/java/com/mor/emailservice/kafka/OrderConsumer.java
@@ -1,5 +1,13 @@
 package com.mor.emailservice.kafka;
 
+/*
+    @KafkaListener allows a method to consume messages from Kafka topic(s).
+    topics: The topics for this listener.
+    groupId: Override the group.id property for the consumer factory with this value for this listener only.
+    @KafkaListener designates a method as a listener in a KafkaMessageListenerContainer.
+    A KafkaMessageListenerContainer is how Spring Boot connects and polls records from Kafka under the hood.
+*/
+
 import com.mor.basedomains.dto.OrderEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
